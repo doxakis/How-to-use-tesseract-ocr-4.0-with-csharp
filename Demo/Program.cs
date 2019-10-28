0dc13d7ae6fc0a179fd3d05773695dbee99538e0
Philip Doxakis <doxakis@users.noreply.github.com>
2019-10-28 07:29:26 -0400

    style: fix indentation

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,75 +5,76 @@ using System.Threading.Tasks;
 
 namespace Demo
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
 			Stopwatch stopwatch = new Stopwatch();
 			stopwatch.Start();
 
-            var solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+			var solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
 
-            var tesseractPath = solutionDirectory + @"\tesseract-master.1153";
-            var testFiles = Directory.EnumerateFiles(solutionDirectory + @"\samples");
+			var tesseractPath = solutionDirectory + @"\tesseract-master.1153";
+			var testFiles = Directory.EnumerateFiles(solutionDirectory + @"\samples");
 
-            var maxDegreeOfParallelism = Environment.ProcessorCount;
-            Parallel.ForEach(testFiles, new ParallelOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism }, (fileName) =>
-            {
-                var imageFile = File.ReadAllBytes(fileName);
-                var text = ParseText(tesseractPath, imageFile, "eng", "fra");
-                Console.WriteLine("File:" + fileName + "\n" + text + "\n");
-            });
+			var maxDegreeOfParallelism = Environment.ProcessorCount;
+			Parallel.ForEach(testFiles, new ParallelOptions {MaxDegreeOfParallelism = maxDegreeOfParallelism}, (fileName) =>
+			{
+				var imageFile = File.ReadAllBytes(fileName);
+				var text = ParseText(tesseractPath, imageFile, "eng", "fra");
+				Console.WriteLine("File:" + fileName + "\n" + text + "\n");
+			});
 
 			stopwatch.Stop();
 			Console.WriteLine("Duration: " + stopwatch.Elapsed);
 			Console.WriteLine("Press enter to continue...");
-            Console.ReadLine();
-        }
+			Console.ReadLine();
+		}
 
-        private static string ParseText(string tesseractPath, byte[] imageFile, params string[] lang)
-        {
-            string output = string.Empty;
-            var tempOutputFile = Path.GetTempPath() + Guid.NewGuid();
-            var tempImageFile = Path.GetTempFileName();
+		private static string ParseText(string tesseractPath, byte[] imageFile, params string[] lang)
+		{
+			string output = string.Empty;
+			var tempOutputFile = Path.GetTempPath() + Guid.NewGuid();
+			var tempImageFile = Path.GetTempFileName();
 
-            try
-            {
-                File.WriteAllBytes(tempImageFile, imageFile);
+			try
+			{
+				File.WriteAllBytes(tempImageFile, imageFile);
 
-                ProcessStartInfo info = new ProcessStartInfo();
-                info.WorkingDirectory = tesseractPath;
-                info.WindowStyle = ProcessWindowStyle.Hidden;
-                info.UseShellExecute = false;
-                info.FileName = "cmd.exe";
-                info.Arguments =
-                    "/c tesseract.exe " +
-                    // Image file.
-                    tempImageFile + " " +
-                    // Output file (tesseract add '.txt' at the end)
-                    tempOutputFile +
-                    // Languages.
-                    " -l " + string.Join("+", lang);
+				ProcessStartInfo info = new ProcessStartInfo();
+				info.WorkingDirectory = tesseractPath;
+				info.WindowStyle = ProcessWindowStyle.Hidden;
+				info.UseShellExecute = false;
+				info.FileName = "cmd.exe";
+				info.Arguments =
+					"/c tesseract.exe " +
+					// Image file.
+					tempImageFile + " " +
+					// Output file (tesseract add '.txt' at the end)
+					tempOutputFile +
+					// Languages.
+					" -l " + string.Join("+", lang);
 
-                // Start tesseract.
-                Process process = Process.Start(info);
-                process.WaitForExit();
-                if (process.ExitCode == 0)
-                {
-                    // Exit code: success.
-                    output = File.ReadAllText(tempOutputFile + ".txt");
-                }
-                else
-                {
-                    throw new Exception("Error. Tesseract stopped with an error code = " + process.ExitCode);
-                }
-            }
-            finally
-            {
-                File.Delete(tempImageFile);
-                File.Delete(tempOutputFile + ".txt");
-            }
-            return output;
-        }
-    }
+				// Start tesseract.
+				Process process = Process.Start(info);
+				process.WaitForExit();
+				if (process.ExitCode == 0)
+				{
+					// Exit code: success.
+					output = File.ReadAllText(tempOutputFile + ".txt");
+				}
+				else
+				{
+					throw new Exception("Error. Tesseract stopped with an error code = " + process.ExitCode);
+				}
+			}
+			finally
+			{
+				File.Delete(tempImageFile);
+				File.Delete(tempOutputFile + ".txt");
+			}
+
+			return output;
+		}
+	}
 }
